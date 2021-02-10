77ce29cb11bf4611a17bd34fb3831ce29f18282e
Bushona <roskoteq@gmail.com>
2021-02-10 09:35:02 +0200

    Add Visual Studio project

---
+++ 10.02.2021/10.02.2021/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._02._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма която чете средният успех на учениците от една група и извжда на класът
+            Console.WriteLine("Колко ученици има?");
+            int n = int.Parse(Console.ReadLine());
+            double obshto;
+            double suma = 0;
+            for (int i = 0; i <n; i++)
+            {
+                Console.WriteLine("Въведете успеха: ");
+                double u = double.Parse(Console.ReadLine());
+                suma = suma + u;
+            }
+            obshto = suma / n;
+            Console.WriteLine("Общият успех е:{0:f2}",obshto);
+        }
+    }
+}
