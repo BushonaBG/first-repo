4a5a76e97595d5dcc83354f46c7c3079394eb0e4
Bushona <roskoteq@gmail.com>
2021-02-17 09:37:44 +0200

    dobavqm tema 2

---
+++ 17.02.2021/17.02.2021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._02._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi dvoichno chislo: ");
+            string bin = Console.ReadLine();
+            int dec = Convert.ToInt32(bin, 2);
+            Console.WriteLine("Desetichnoto: " + dec);
+            Console.WriteLine("Dvoichno: " + bin);
+        }
+    }
+}
