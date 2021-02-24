b58a8e47f2b3334466f918554cbfcbb0bfb5da42
Bushona <roskoteq@gmail.com>
2021-02-24 09:48:13 +0200

    Dobavqm zadachite ot 24.02.2021

---
+++ 24.02.2021/24.02.2021/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._02._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете десетично число");
+            int chislo = int.Parse(Console.ReadLine());
+            string chislo2 = Convert.ToString(chislo, 2);
+            Console.WriteLine("Desetichno: " + chislo);
+            Console.WriteLine("Dvoichno: " + chislo2);
+            string chislo16 = Convert.ToString(chislo, 16);
+            Console.WriteLine("Shestnaisetichno: " + chislo16);
+        }
+    }
+}
