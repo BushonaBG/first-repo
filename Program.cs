11df30d7949f2229a397c89fd5e4207069d0a211
BushonaBG <78840780+BushonaBG@users.noreply.github.com>
2021-09-20 10:27:18 +0300

    Broyni sistemi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете десетично число");
+            int chislo = int.Parse(Console.ReadLine());
+            string dvuichno = Convert.ToString(chislo,2);
+            Console.WriteLine("Двуичното число е: {0}",dvuichno);
+        }
+    }
+}
